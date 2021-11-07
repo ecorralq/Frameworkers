19220f753af4e9e84e34ba1113a21975d57e9431
ecorralq <ecorralq@uoc.edu>
2021-11-07 18:17:07 +0100

    commit

---
+++ src/app/lista.ts
@@ -0,0 +1,15 @@
+import { Cancion } from './cancion';
+
+export const Lista: Cancion[] = [
+  { id:1, titulo:'Highway to Hell', autor:'AC/DC',fecha:1979, album:'Highway to Hell', estilo:'Hard Rock'},
+  { id:2, titulo:'Wind of Change', autor:'Scorpions',fecha:1990, album:'Crazy World', estilo:'Hard Rock'},
+  { id:3, titulo:'Knockin on Heavens Door', autor:'Bob Dylan',fecha:1973, album:'Pat Garrett and Billy the Kid', estilo:'Folk Rock'},
+  { id:4, titulo:'Thriller', autor:'Michael Jackson',fecha:1983, album:'Thriller', estilo:'album'},
+  { id:5, titulo:'Mediterraneo', autor:'Joan Manuel Serrat',fecha:1971, album:'Mediterraneo', estilo:'Folk'},
+  { id:6, titulo:'Libre', autor:'Nino Bravo',fecha:1972, album:'Libre', estilo:'Pop'},
+  { id:7, titulo:'Fear of the Dark', autor:'Iron Maiden',fecha:1992, album:'Fear of the Dark', estilo:'Heavy Metal'},
+  { id:8, titulo:'The Dark Side of the Moon', autor:'Pink Floyd',fecha:1973, album:'The Dark Side of the Moon', estilo:'Progressive Rock'},
+  { id:9, titulo:'Hocus Pocus', autor:'Focus',fecha:1971, album:'Focus II', estilo:'Hard Rock'},
+  { id:10, titulo:'Layla', autor:'Eric Clapton',fecha:1971, album:'Layla and Other Assorted Love Songs', estilo:'Rock'},
+  { id:11, titulo:'The Final Countdown', autor:'Europe',fecha:1986, album:'The Final Countdown', estilo:'Hard Rock'}
+];
---
+++ src/app/lista/lista.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Cancion } from '../cancion';
+import { Lista } from '../lista';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+
+export class ListaComponent implements OnInit {
+
+  cancion=Lista;
+
+  selectedCancion?: Cancion;
+
+  onSelect(cancion: Cancion): void {
+    this.selectedCancion = cancion;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+}
