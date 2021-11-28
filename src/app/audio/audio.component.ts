7cebb352d64c712c5ae5d3edadcdb57fe9dc2aa0
ecorralq <ecorralq@uoc.edu>
2021-11-28 11:49:48 +0100

    Commit

---
+++ src/app/audio/audio.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Cancion } from '../cancion';
+
+@Component({
+  selector: 'app-audio',
+  templateUrl: './audio.component.html',
+  styleUrls: ['./audio.component.css']
+})
+export class AudioComponent implements OnInit {
+
+  @Input() cancion!: Cancion;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
